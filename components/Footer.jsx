e8f15692197e2aba9f743b2d8125586677ca29f0
ahmedamrzezo <ahmedamrzezo@gmail.com>
2021-12-01 18:18:39 +0200

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
 	return (
 		<footer className="bg-primary_light_color primary_color p-4 w-full fixed bottom-0">
-			Thanks for watching
+			<div className="container mx-auto">Thanks for watching</div>
 		</footer>
 	);
 }
