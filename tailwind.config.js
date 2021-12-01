a0164113e26265c74a4ca96cf1b01148c7b6b609
ahmedamrzezo <ahmedamrzezo@gmail.com>
2021-12-01 17:50:04 +0200

    tailwind setup and general styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,28 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  mode: 'jit',
+  // mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary_color: '#435B9C',
+      secondary_color: '#6AA09A',
+      primary_color_50: 'rgb(67, 67, 156 / 50%)',
+      primary_light_color: '#e3f1ee',
+      primary_light_color_80: 'rgba(227, 241, 238, 0.8)',
+      primary_dark_color: '#113537',
+      white: '#fff',
+      gray: colors.trueGray,
+      success: colors.emerald[500],
+      danger: colors.red[600],
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
