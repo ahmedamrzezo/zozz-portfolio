015fd67cb50883c2bc0738e97d2a179bbc40eb0e
ahmedamrzezo <ahmedamrzezo@gmail.com>
2021-12-23 13:50:08 +0200

    fix connecting

--- pages/api/projects/[projectId].js
+++ pages/api/projects/[projectId].js
@@ -1,11 +1,13 @@
-require('../../../middleware/mongoose');
-const Project = require('../../../models/project.model');
+import dbConnect from '../../../middleware/mongoose';
+import Project from '../../../models/project.model';
 
 export default async function projectDetail(req, res) {
+	await dbConnect();
 	try {
 		const project = await Project.findById(req.query.projectId);
 		res.status(200).json(project);
 	} catch (error) {
+		console.log(error);
 		res.status(404).send();
 	}
 
