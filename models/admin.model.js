49b3eb89e6fecaa073e794a496b57455ee77f993
ahmedamrzezo <ahmedamrzezo@gmail.com>
2022-01-10 00:10:14 +0200

    hash password

--- models/admin.model.js
+++ models/admin.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const schema = new mongoose.Schema({
 	username: {
@@ -13,6 +14,32 @@ const schema = new mongoose.Schema({
 		minlength: 8
 	}
 });
+
+schema.methods.toJSON = function () {
+	const user = this.toObject({
+		virtuals: true,
+	});
+
+	delete user.password;
+	delete user.tokens;
+	delete user.name;
+	delete user._id;
+	delete user.__v;
+
+	return user;
+};
+
+schema.pre('save', async function(next) {
+	const admin = this;
+
+	if (admin.isModified('password')) {
+		const saltRounds = 10;
+		admin.password = await bcrypt.hash(admin.password, saltRounds);
+	}
+	console.log(admin)
+
+	next();
+})
 
 const Admin = mongoose.models.Admin || mongoose.model('Admin', schema);
 
